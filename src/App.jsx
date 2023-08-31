8b756aa71ef2d3065c1fbbde645547ed58cbadbc
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-08-31 02:12:06 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import { logo } from "./assets";
+import { Home, CreatePost } from "./pages";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-[#250434] sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28 object-contain" />
+        </Link>
+
+        <Link
+          to="/create-post"
+          className="font-inter font-bold bg-[#15efaa] text-[#250434] px-4 py-2 rounded-md"
+        >
+          Create
+        </Link>
+      </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#110218] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+
+import { Loader, Card, FormField } from "../components";
+
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0)
+    return data.map((post) => <Card key={post._id} {...post} />);
+};
+
+const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
+  const [searchText, setSearchText] = useState("");
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#faf1fe] text-[32px]">
+          Made by the community
+        </h1>
+        <p className="mt-2 text-[#faf1fe] text-[16px] max-w-[500px]">
+          Browse through the recently generated images by our community
+        </p>
+      </div>
+
+      <div className="mt-16">
+        <FormField />
+      </div>
+
+      <div className="mt-10">
+        {loading ? (
+          <div className="flex justify-center items-center">
+            <Loader />
+          </div>
+        ) : (
+          <>
+            {searchText && (
+              <h2 className="font-medium text-[#faf1fe] text-xl mb-3">
+                Showing results for{" "}
+                <span className="text-[#15efaa]">{searchText}</span>
+              </h2>
+            )}
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3"></div>
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Home;

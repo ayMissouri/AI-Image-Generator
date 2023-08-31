12ffda17c6c8f55f8b32fa12ef8bbcd381246fad
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-08-31 02:59:47 +0100

    FormField styling

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 
-function FormField() {
-  return <div>FormField</div>;
-}
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb-2">
+        <label htmlFor={name} className="block text-sm font-medium text-white">
+          {labelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#15efaa] py-1 px-2 rounded-[5px] text-black"
+          >
+            Surprise me
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-[#250434] border border-[#f6d283] text-gray-900 text-sm rounded-lg focus:ring-[#15efaa] focus:border-[#15efaa] outline-none block w-full p-3"
+      />
+    </div>
+  );
+};
 
 export default FormField;

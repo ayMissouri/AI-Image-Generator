dee13592667552cc03fd7df0e6a7b4c0e5cc03ae
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-08-31 02:30:05 +0100

    Homepage Rendercards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,13 @@ import React, { useState, useEffect } from "react";
 import { Loader, Card, FormField } from "../components";
 
 const RenderCards = ({ data, title }) => {
-  if (data?.length > 0)
+  if (data?.length > 0) {
     return data.map((post) => <Card key={post._id} {...post} />);
+  }
+
+  return (
+    <h2 className="mt-5 font-bold text-[#15efaa] text-xl uppercase">{title}</h2>
+  );
 };
 
 const Home = () => {
@@ -41,7 +46,13 @@ const Home = () => {
                 <span className="text-[#15efaa]">{searchText}</span>
               </h2>
             )}
-            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3"></div>
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+              {searchText ? (
+                <RenderCards data={[]} title="No search results found" />
+              ) : (
+                <RenderCards data={[]} title="No posts found" />
+              )}
+            </div>
           </>
         )}
       </div>
